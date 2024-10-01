ddc24dc31e7678482d54b5bdb4b3f459a4755ac3
Rassul-creator-commits <181149570+Rassul-creator-commits@users.noreply.github.com>
2024-09-30 22:12:54 -0400

    Create 24.cpp

---
+++ Khalizov_Rassul_Lab_3_9/30/24.cpp
@@ -0,0 +1,40 @@
+// Rassul Khalizov
+// Lab 3
+// 9/30/24
+
+#include<iostream>
+using namespace std;
+#include<iomanip>
+#include<string>
+#include <iostream>
+
+using namespace std;
+int main() // uses the main
+{
+    cout << setw(86) << setfill('#') << ('#') << endl;                                                                                  
+    cout << "#" << setfill(' ') << setw(85) << "#" << endl;                               
+    cout << "#Ways to access the Task Manager on your Windows computer:" << setfill(' ') << setw(28) << "#" << endl;
+    cout << "#" << setfill(' ') << setw(85) << "#" << endl;                               
+    cout << ('#') << setfill(' ') << setw(59) << "Press the key combination Ctrl + Shift + Escape" << setfill(' ') << setw(26) << "#" << endl;
+    cout << "#" << setfill(' ') << setw(85) << "#" << endl;                               
+    cout << "#" << setfill(' ') << setw(86) << "Press the key combination Ctrl + Alt + Delete and select  “Task Manager\"" << "#" << endl; 
+    cout << "#" << setfill(' ') << setw(85) << "#" << endl;                               
+    cout << "#" << setfill(' ') << setw(68) << "Type “Task Manager”\ in the Windows Start menu search" << setfill(' ') << setw(21) << "#" << endl;
+    cout << setw(10) << "#                                                                                    #" << endl;
+    cout << setw(86) << setfill('#') << ('#') << endl;                                                                                  
+    
+    /*
+######################################################################################
+#                                                                                    #
+#Ways to access the Task Manager on your Windows computer:                           #
+#                                                                                    #
+#            Press the key combination Ctrl + Shift + Escape                         #
+#                                                                                    #
+#            Press the key combination Ctrl + Alt + Delete and select  “Task Manager"#
+#                                                                                    #
+#            Type “Task Manager” in the Windows Start menu search                    #
+#                                                                                    #
+######################################################################################
+    */
+    return 0;
+}
